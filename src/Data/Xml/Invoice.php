09fc9c0398ec2863f6443cdb8a2fddcb95865123
Alejandro Mostajo <amostajo@gmail.com>
2019-03-21 14:16:02 -0600

    Localized invoice model.

--- src/Data/Xml/Invoice.php
+++ src/Data/Xml/Invoice.php
@@ -94,69 +94,69 @@ class Invoice extends Model
     public function isValid()
     {
         if ($this->currency === null || strlen($this->currency) === 0)
-            throw new Exception('Currency is missing.');
+            throw new Exception(__i18n('Currency is missing.'));
         if ($this->saleType === null || strlen($this->saleType) === 0)
-            throw new Exception('Sale type is missing.');
+            throw new Exception(__i18n('Sale type is missing.'));
         if (!SaleType::exists($this->saleType))
-            throw new Exception(sprintf('Unknown sale type \'%s\'.', $this->saleType));
+            throw new Exception(sprintf(__i18n('Unknown sale type \'%s\'.'), $this->saleType));
         if ($this->saleType === SaleType::CREDIT
             && ($this->creditTerms === null || strlen($this->creditTerms) === 0)
         )
-            throw new Exception('Credit terms is required if sale type is set for \'CREDIT\'.');
+            throw new Exception(__i18n('Credit terms is required if sale type is set for \'CREDIT\'.'));
         if ($this->saleType === SaleType::CREDIT && strlen($this->creditTerms) > 10)
-            throw new Exception('Credit terms has more than 10 characters.');
+            throw new Exception(__i18n('Credit terms has more than 10 characters.'));
         if ($this->paymentType === null || strlen($this->paymentType) === 0)
-            throw new Exception('Payment type is missing.');
+            throw new Exception(__i18n('Payment type is missing.'));
         if (!PaymentType::exists($this->paymentType))
-            throw new Exception(sprintf('Unknown payment type \'%s\'.', $this->paymentType));
+            throw new Exception(sprintf(__i18n('Unknown payment type \'%s\'.'), $this->paymentType));
         if ($this->exchangeRate && !is_numeric($this->exchangeRate))
-            throw new Exception('Exchange rate is not numeric.');
+            throw new Exception(__i18n('Exchange rate is not numeric.'));
         if ($this->exchangeRate && $this->exchangeRate > 9999999999999.99999)
-            throw new Exception('Exchange rate should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Exchange rate should be lower than 9999999999999.99999.'));
         if ($this->totalTaxedServices && !is_numeric($this->totalTaxedServices))
-            throw new Exception('Total taxed services is not numeric.');
+            throw new Exception(__i18n('Total taxed services is not numeric.'));
         if ($this->totalTaxedServices && $this->totalTaxedServices > 9999999999999.99999)
-            throw new Exception('Total taxed services should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total taxed services should be lower than 9999999999999.99999.'));
         if ($this->totalExemptServices && !is_numeric($this->totalExemptServices))
-            throw new Exception('Total exempt services is not numeric.');
+            throw new Exception(__i18n('Total exempt services is not numeric.'));
         if ($this->totalExemptServices && $this->totalExemptServices > 9999999999999.99999)
-            throw new Exception('Total exempt services should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total exempt services should be lower than 9999999999999.99999.'));
         if ($this->totalTaxedGoods && !is_numeric($this->totalTaxedGoods))
-            throw new Exception('Total taxed goods is not numeric.');
+            throw new Exception(__i18n('Total taxed goods is not numeric.'));
         if ($this->totalTaxedGoods && $this->totalTaxedGoods > 9999999999999.99999)
-            throw new Exception('Total taxed goods should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total taxed goods should be lower than 9999999999999.99999.'));
         if ($this->totalExemptGoods && !is_numeric($this->totalExemptGoods))
-            throw new Exception('Total exempt goods is not numeric.');
+            throw new Exception(__i18n('Total exempt goods is not numeric.'));
         if ($this->totalExemptGoods && $this->totalExemptGoods > 9999999999999.99999)
-            throw new Exception('Total exempt goods should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total exempt goods should be lower than 9999999999999.99999.'));
         if ($this->totalTaxed && !is_numeric($this->totalTaxed))
-            throw new Exception('Total taxed is not numeric.');
+            throw new Exception(__i18n('Total taxed is not numeric.'));
         if ($this->totalTaxed && $this->totalTaxed > 9999999999999.99999)
-            throw new Exception('Total taxed should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total taxed should be lower than 9999999999999.99999.'));
         if ($this->totalExempt && !is_numeric($this->totalExempt))
-            throw new Exception('Total exempt is not numeric.');
+            throw new Exception(__i18n('Total exempt is not numeric.'));
         if ($this->totalExempt && $this->totalExempt > 9999999999999.99999)
-            throw new Exception('Total exempt should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total exempt should be lower than 9999999999999.99999.'));
         if ($this->totalSales && !is_numeric($this->totalSales))
-            throw new Exception('Total sales is not numeric.');
+            throw new Exception(__i18n('Total sales is not numeric.'));
         if ($this->totalSales && $this->totalSales > 9999999999999.99999)
-            throw new Exception('Total sales should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total sales should be lower than 9999999999999.99999.'));
         if ($this->totalDiscount && !is_numeric($this->totalDiscount))
-            throw new Exception('Total discount is not numeric.');
+            throw new Exception(__i18n('Total discount is not numeric.'));
         if ($this->totalDiscount && $this->totalDiscount > 9999999999999.99999)
-            throw new Exception('Total discount should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total discount should be lower than 9999999999999.99999.'));
         if ($this->totalNetSales && !is_numeric($this->totalNetSales))
-            throw new Exception('Total net sales is not numeric.');
+            throw new Exception(__i18n('Total net sales is not numeric.'));
         if ($this->totalNetSales && $this->totalNetSales > 9999999999999.99999)
-            throw new Exception('Total net sales should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total net sales should be lower than 9999999999999.99999.'));
         if ($this->totalTaxes && !is_numeric($this->totalTaxes))
-            throw new Exception('Total in taxes is not numeric.');
+            throw new Exception(__i18n('Total in taxes is not numeric.'));
         if ($this->totalTaxes && $this->totalTaxes > 9999999999999.99999)
-            throw new Exception('Total in taxes should be lower than 9999999999999.99999.');
+            throw new Exception(__i18n('Total in taxes should be lower than 9999999999999.99999.'));
         if ($this->reference && !is_a($this->reference, Reference::class))
             throw new Exception(__i18n('Reference must be an instance of class \'Reference\'.'));
         if ($this->normative === null || strlen($this->normative) === 0)
-            throw new Exception('Normative is missing.');
+            throw new Exception(__i18n('Normative is missing.'));
         if ($this->normative && !is_a($this->normative, Normative::class))
             throw new Exception(__i18n('Normative must be an instance of class \'Normative\'.'));
         return parent::isValid();
